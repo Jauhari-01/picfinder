12f199e1a468cc9933a26b8ea8f72eea585d272a
Jauhari-01 <shubham.jauhari009@gmail.com>
2023-09-05 16:28:37 +0530

    Created project Structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBar from "./Component/SearchBar";
+
+const App = ()=>{
+    return(
+        <>
+            <SearchBar />
+        </>
+    );
+}
+
+export default App;
